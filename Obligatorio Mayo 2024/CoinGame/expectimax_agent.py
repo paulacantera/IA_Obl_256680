9bc5651a3a2c7320950c06d86ffed6bd4d4d5ee4
paulacantera <paulacanteramontero@hotmail.com>
2024-07-05 20:34:19 -0300

    minimax implemented bad heuristic

--- Obligatorio Mayo 2024/CoinGame/expectimax_agent.py
+++ Obligatorio Mayo 2024/CoinGame/expectimax_agent.py
@@ -7,28 +7,44 @@ class ExpectimaxAgent(Agent):
     def __init__(self, player=1, max_depth=3):
         super().__init__(player)
         self.max_depth = max_depth
-        
+
     def next_action(self, obs):
-        action, _ = self.expectimax(obs, self.player, True, self.max_depth)
+        action, _ = self.expectimax(obs, self.player, self.max_depth, True)
         return action
-    
+
     def heuristic_utility(self, board: Board):
-        # heuristica simple q se basa en la cant de coins restantes
-        return board.grid.sum()
-    
-    def expectimax(self, board: Board, player, is_maximizing, depth):
-        if depth == 0 or board.is_end(player)[0]:
+        total_coins = board.grid.sum()
+        sequences = self.count_sequences(board.grid)
+        return -total_coins + sequences
+
+    def count_sequences(self, grid):
+        sequences = 0
+        for row in grid:
+            in_sequence = False
+            for cell in row:
+                if cell == 1 and not in_sequence:
+                    sequences += 1
+                    in_sequence = True
+                elif cell == 0:
+                    in_sequence = False
+        return sequences
+
+    def expectimax(self, board: Board, player, depth, is_maximizing):
+        if board.is_end(player)[0] or depth == 0:
+            if board.is_end(player)[1] == self.player:
+                return None, 1  #win
+            elif board.is_end(player)[1] != 0:
+                return None, -1  #loss
             return None, self.heuristic_utility(board)
-        
+
         possible_actions = board.get_possible_actions()
-        
         if is_maximizing:
             best_value = -math.inf
             best_action = None
             for action in possible_actions:
                 new_board = board.clone()
                 new_board.play(action)
-                _, value = self.expectimax(new_board, (player % 2) + 1, False, depth - 1)
+                _, value = self.expectimax(new_board, (player % 2) + 1, depth - 1, False)
                 if value > best_value:
                     best_value = value
                     best_action = action
@@ -38,6 +54,6 @@ class ExpectimaxAgent(Agent):
             for action in possible_actions:
                 new_board = board.clone()
                 new_board.play(action)
-                _, value = self.expectimax(new_board, (player % 2) + 1, True, depth - 1)
+                _, value = self.expectimax(new_board, (player % 2) + 1, depth - 1, True)
                 expected_value += value / len(possible_actions)
             return None, expected_value
---
+++ Obligatorio Mayo 2024/CoinGame/minimax_agent.py
@@ -0,0 +1,49 @@
+from agent import Agent
+from board import Board
+import math
+
+class MinimaxAgent(Agent):
+    
+    def __init__(self, player=1, max_depth=3):
+        super().__init__(player)
+        self.max_depth = max_depth
+        
+    def next_action(self, obs):
+        action, _ = self.minimax(obs, self.player, True, self.max_depth)
+        return action
+    
+    def heuristic_utility(self, board: Board):
+        return board.grid.sum()
+    
+    def minimax(self, board: Board, player, is_maximizing, depth):
+        if board.is_end(player)[0] or depth == 0:
+            if board.is_end(player)[0]:
+                if board.is_end(player)[1] == self.player:
+                    return None, 1 #win
+                else:
+                    return None, -1 #loss
+            return None, self.heuristic_utility(board)
+
+        possible_actions = board.get_possible_actions()
+        if is_maximizing:
+            best_value = -math.inf
+            best_action = None
+            for action in possible_actions:
+                new_board = board.clone()
+                new_board.play(action)
+                _, value = self.minimax(new_board, (player % 2) + 1, False, depth - 1)
+                if value > best_value:
+                    best_value = value
+                    best_action = action
+            return best_action, best_value
+        else:
+            best_value = math.inf
+            best_action = None
+            for action in possible_actions:
+                new_board = board.clone()
+                new_board.play(action)
+                _, value = self.minimax(new_board, (player % 2) + 1, True, depth - 1)
+                if value < best_value:
+                    best_value = value
+                    best_action = action
+            return best_action, best_value
